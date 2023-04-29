205b0dd0762bc524e1676b96da0a43ce84b5baa5
namelaise01 <nikoss97115@gmail.com>
2023-04-30 00:00:13 +0200

    add display offers

--- app/src/main/java/dev/munderstand/epsi/HomeActivity.kt
+++ app/src/main/java/dev/munderstand/epsi/HomeActivity.kt
@@ -1,5 +1,6 @@
 package dev.munderstand.epsi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,22 +13,28 @@ class HomeActivity : BaseActivity() {
 
     val tab1Fragment = Fragment1.newInstance("", "")
     val tab2Fragment = Fragment2.newInstance("", "")
+    val tab3Fragment = Fragment2.newInstance("", "")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         val textViewTab1 = findViewById<TextView>(R.id.textViewTabMap)
-        val textViewTab2 = findViewById<TextView>(R.id.textViewTabStore)
+        val textViewTab2 = findViewById<TextView>(R.id.textViewOffer)
+        val textViewTab3 = findViewById<TextView>(R.id.textViewTabStore)
 
 
         setHeaderTxt("Logo")
         showBack()
 
         textViewTab1.setOnClickListener(View.OnClickListener {
-            showTab1()
+            val intent = Intent(application, OfferActivity::class.java)
+            startActivity(intent)
         })
         textViewTab2.setOnClickListener(View.OnClickListener {
             showTab2()
+        })
+        textViewTab3.setOnClickListener(View.OnClickListener {
+            showTab3()
         })
     }
 
@@ -47,4 +54,11 @@ class HomeActivity : BaseActivity() {
         fragmentTransaction.commit()
     }
 
+    fun showTab3() {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.addToBackStack("Tab3")
+        fragmentTransaction.replace(R.id.fragmentContent, tab3Fragment)
+        fragmentTransaction.commit()
+    }
 }
---
+++ app/src/main/java/dev/munderstand/epsi/Offers.kt
@@ -0,0 +1,9 @@
+package dev.munderstand.epsi
+
+class Offers(
+    val name: String,
+    val description: String,
+    val picture_url: String,
+
+    ) {
+}
