bec93ce688dd06de79f5fb367f2b484635402de8
Mundersatnd <munderstand101@gmail.com>
2023-04-30 14:23:03 +0200

    Modification Singin activity

--- app/src/main/java/dev/munderstand/epsi/registration/SignInActivity.kt
+++ app/src/main/java/dev/munderstand/epsi/registration/SignInActivity.kt
@@ -1,12 +1,17 @@
-package dev.munderstand.epsi
+package dev.munderstand.epsi.registration
 
+
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import dev.munderstand.epsi.R
+import dev.munderstand.epsi.common.BaseActivity
+
 
 class SignInActivity : BaseActivity() {
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
@@ -14,11 +19,19 @@ class SignInActivity : BaseActivity() {
         val buttonRegistrationButton = findViewById<Button>(R.id.registrationButton)
 
 
-        setHeaderTxt("Création de compte")
-        showBack()
+        setHeaderTxt("Bienvenue !")
+       // showBack()
+
+/*
+        val scanButton = findViewById<Button>(R.id.scan_button)
+        scanButton.setOnClickListener {
+            val intent = Intent(this@HomeActivity, QRCodeScanActivity::class.java)
+            startActivity(intent)
+        }
+        */
 
         buttonQrCodeScan.setOnClickListener(View.OnClickListener {
-            val intent = Intent(application, QrCodeScanActivity::class.java)
+            val intent = Intent(application, QRCodeScannerActivity::class.java)
               startActivity(intent)
         })
 
